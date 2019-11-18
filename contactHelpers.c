f5d7e9893c3132e3ec5872dc9b7b88536659eb2d
pereiradaniel <danielpaulpereira@protonmail.com>
2019-11-18 16:38:43 -0500

    Add project files.

---
+++ contactHelpers.c
@@ -0,0 +1,35 @@
+//==============================================
+// Name:           Full name here
+// Student Number: #########
+// Email:          userID@myseneca.ca
+// Section:        XXX
+// Date:           
+//==============================================
+// Assignment:     2
+// Milestone:      2
+//==============================================
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include "contactHelpers.h"
+
+// Function Definitions
+
+// Clear the standard input buffer
+void clearKeyboard(void)
+{
+    while (getchar() != '\n')   ; // empty execution code block on purpose
+}
+
+void pause(void) {}
+
+int getInt(void) {}
+
+int getIntInRange(int min, int max) {}
+
+int yes(void) {}
+
+int menu(void) {}
+
+void contactManagerSystem(void) {}
