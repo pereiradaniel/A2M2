90731a67fee31a3a5d748d1b58f9331eaca128da
Daniel Paul Grech Pereira <13129650+pereiradaniel@users.noreply.github.com>
2019-11-18 20:19:35 -0500

    Up to speed.  Begin working on pause function

--- contactHelpers.c
+++ contactHelpers.c
@@ -10,7 +10,6 @@
 //==============================================
 
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include "contactHelpers.h"
 
--- contacts.c
+++ contacts.c
@@ -10,55 +10,94 @@
 //==============================================
 
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
+#include "contacts.h"
 
-// This source file needs to "know about" the structures you declared
-// in the header file before referring to those new types:
-// HINT: put the header file name in double quotes so the compiler knows
-//       to look for it in the same directory/folder as this source file
-// #include your contacts header file on the next line:
+void getName(struct Name* name)
+{
+	char yorn;
+	// Ask for first name:
+	printf("Please enter the contact's first name: ");
+	scanf(" %30[^\n]", name->firstName);
 
+	// Ask for middle initial:
+	printf("Do you want to enter a middle initial(s)? (y or n): ");
+	scanf(" %c", &yorn);
+	if (yorn == 'y' || yorn == 'Y')
+	{
+		printf("Please enter the contact's middle initial(s): ");
+		scanf(" %7[^\n]", name->middleInitial);
+	}
 
+	// Ask for last name:
+	printf("Please enter the contact's last name: ");
+	scanf(" %35[^\n]", name->lastName);
+}
+void getAddress(struct Address* address)
+{
+	char yorn;
+	// Ask for street number:
+	//	! Enforce values greater than 0
+	do
+	{
+		printf("Please enter the contact's street number: ");
+		scanf(" %d", &address->streetNumber);
+	} while (address->streetNumber < 1);
 
-// This source file needs to "know about" the functions you prototyped
-//         in the contact helper header file too
-// HINT-1: You will want to use the new yes() and getInt() functions to help
-//         simplify the data input process and reduce redundant coding
-//
-// HINT-2: Put the header file name in double quotes so the compiler knows
-//         to look for it in the same directory/folder as this source file
-//         #include your contactHelpers header file on the next line:
+	// Ask for street name:
+	printf("Please enter the contact's street name: ");
+	scanf(" %40[^\n]", address->street);
 
+	// Ask for apartment number:
+	printf("Do you want to enter an apartment number? (y or n): ");
+	scanf(" %c", &yorn);
+	if (yorn == 'y' || yorn == 'Y')
+	{
+		do
+		{
+			printf("Please enter the contact's apartment number: ");
+			scanf(" %d", &address->apartmentNumber);
+		} while (address->apartmentNumber < 1);
+	}
 
+	// Ask for postal code:
+	printf("Please enter the contact's postal code: ");
+	scanf(" %7[^\n]", address->postalCode);
 
-// +-------------------------------------------------+
-// | NOTE:  Copy/Paste your Assignment-2 Milestone-1 |
-// |        function definitions below...            |
-// +-------------------------------------------------+
-//
-// HINT:  Update these function to use the helper 
-//        functions where applicable (ex: yes() and getInt() )
+	// Ask for city:
+	printf("Please enter the contact's city: ");
+	scanf(" %40[^\n]", address->city);
 
-// getName:
+}
+void getNumbers(struct Numbers* numbers)
+{
+	char yorn;
+	// Ask for cell number:
+	printf("Do you want to enter a cell phone number? (y or n): ");
+	scanf(" %c", &yorn);
+	if (yorn == 'y' || yorn == 'Y')
+	{
+		printf("Please enter the contact's cell phone number: ");
+		scanf(" %10s", numbers->cell);
+	}
 
+	// Ask for home number:
+	printf("Do you want to enter a home phone number? (y or n): ");
+	scanf(" %c", &yorn);
+	if (yorn == 'y' || yorn == 'Y')
+	{
+		printf("Please enter the contact's home phone number: ");
+		scanf(" %10s", numbers->home);
+	}
 
+	// Ask for business number:
+	printf("Do you want to enter a business phone number? (y or n): ");
+	scanf(" %c", &yorn);
+	if (yorn == 'y' || yorn == 'Y')
+	{
+		printf("Please enter the contact's business phone number: ");
+		scanf(" %10s", numbers->business);
+	}
+}
 
-// getAddress:
-
-
-
-// getNumbers:
-// NOTE:  Also modify this function so the cell number is
-//        mandatory (don't ask to enter the cell number)
-
-
-
-// +-------------------------------------------------+
-// | NOTE:  Copy/Paste your Assignment-2 Milestone-1 |
-// |        empty function definitions below and     |
-// |        complete the definitions as per the      |
-// |        Milestone-2 specifications               | 
-// +-------------------------------------------------+
-
-// getContact
+void getContact(struct Contact* contact) {}
